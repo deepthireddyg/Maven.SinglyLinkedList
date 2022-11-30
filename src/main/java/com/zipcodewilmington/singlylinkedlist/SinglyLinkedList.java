b62103df8a7b61b2f2d0386d78b351ea22ae177a
Deepthi Ganji <115901667+deepthireddyg@users.noreply.github.com>
2022-11-29 20:05:44 -0500

    Done

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,133 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import javax.xml.soap.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
-}
+public class SinglyLinkedList<T> {
+    Node head;
+
+    public class Node {
+
+        public T data;
+        public Node next;
+
+        public Node(T data) {
+            this.data = data;
+            next = null;
+
+        }
+
+    }
+       public SinglyLinkedList appendNode(SinglyLinkedList list,T data) {
+            Node newnode = new Node(data);
+            if (list.head == null) {
+                list.head = new Node(data);
+            } else {
+                Node lastNode = head;
+
+                while (lastNode.next != null)
+                    lastNode = lastNode.next;
+                lastNode.next = newnode;
+
+            }
+            return list;
+        }
+       public SinglyLinkedList removeNode(SinglyLinkedList list,T data){
+           Node currentNode=head;
+           Node previousNode=null;
+           if(currentNode!=null && currentNode.data.equals(data)){
+               list.head=currentNode.next;
+               return list;
+           }
+           while (currentNode!=null && !currentNode.data.equals(data)){
+               previousNode=currentNode;
+               currentNode=currentNode.next;
+           }
+           if(currentNode!=null && currentNode.data.equals(data))
+               previousNode.next=currentNode.next;
+
+           return list;
+       }
+    public boolean containsNode(SinglyLinkedList list,T data){
+
+        Node currentNode=head;
+        int count = 0;
+        while(currentNode!=null){
+            count++;
+            currentNode=currentNode.next;
+        }
+        return false;
+    }
+    public int findNodeIndex(SinglyLinkedList list,T data) {
+        int index = -1;
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.data.equals(data)) {
+                index++;
+                return index;
+            }
+            index++;
+            currentNode = currentNode.next;
+
+        }
+        return -1;
+    }
+    public int sizeList(SinglyLinkedList list){
+
+        Node currentNode = head;
+        int count = 0;
+        while(currentNode!= null){
+            count++;
+            currentNode=currentNode.next;
+        }
+        return count;
+    }
+    public Object getNode(SinglyLinkedList lst,int index){
+        Node currentNode = head;
+        int counter=0;
+        while(currentNode!=null){
+            if(counter==index)
+                return currentNode.next;
+        }
+        return null;
+    }
+    public SinglyLinkedList copyList(SinglyLinkedList lstSource){
+        SinglyLinkedList lst = new SinglyLinkedList();
+        Node temp = lstSource.head;
+        while(temp.next!=null){
+            lst.appendNode(lst,temp.data);
+            temp=temp.next;
+        }
+        return lst;
+    }
+    public SinglyLinkedList sortList(SinglyLinkedList lst){
+        int size = lst.sizeList(lst);
+        Node current=lst.head;
+        Node index = null;
+        while(current!=null){
+            index=current.next;
+            while(index!=null){
+                if((Integer) current.data>(Integer) index.data){
+                    T temp = current.data;
+                    current.data = index.data;
+                    index.data=temp;
+
+                }
+                index = index.next;
+            }
+            current = current.next;
+        }
+       return lst;
+    }
+    public String print(SinglyLinkedList list){
+        StringBuilder sb=new StringBuilder();
+        Node currentNode=head;
+        while(currentNode!=null){
+            sb.append(currentNode.data+" ");
+            currentNode=currentNode.next;
+        }
+        return sb.toString();
+    }
+    }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,138 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.Assert;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+    @org.junit.jupiter.api.Test
+   public void appendNode() {
+        Object element="test";
+        SinglyLinkedList objList=new SinglyLinkedList();
+        //When
+        objList.appendNode(objList,element);
+        String actual=objList.print(objList);
+        //Then
+        Assert.assertEquals("test ",actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void removeNode() {
+        Object element="test";
+        SinglyLinkedList objList=new SinglyLinkedList();
+        objList.appendNode(objList,element);
+        //When
+        objList.removeNode(objList,element);
+        String actual=objList.print(objList);
+        //Then
+        Assert.assertEquals("",actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void containsNode() {
+        Object element="test";
+        Object element2="test2";
+        SinglyLinkedList objList=new SinglyLinkedList();
+        objList.appendNode(objList,element);
+        objList.appendNode(objList,element2);
+        boolean expected=false;
+        //When
+        boolean actual=objList.containsNode(objList,"test3");
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void findNodeIndex() {
+        Object element="test";
+        Object element2="test2";
+        Object element3="test3";
+        Object element4="test4";
+        SinglyLinkedList objList=new SinglyLinkedList();
+        objList.appendNode(objList,element);
+        objList.appendNode(objList,element2);
+        objList.appendNode(objList,element3);
+        objList.appendNode(objList,element4);
+        int expected=1;
+        //When
+        int actual=objList.findNodeIndex(objList,"test2");
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void sizeList() {
+        Object element="test";
+        Object element2="test2";
+        Object element3="test3";
+        Object element4="test4";
+        SinglyLinkedList objList=new SinglyLinkedList();
+        objList.appendNode(objList,element);
+        objList.appendNode(objList,element2);
+        objList.appendNode(objList,element3);
+        objList.appendNode(objList,element4);
+        int expected=4;
+        //When
+        int actual= objList.sizeList(objList);
+        //Then
+        Assert.assertEquals(expected,actual);
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public void getNode() {
+//        Object element="test";
+//        Object element2="test2";
+//        Object element3="test3";
+//        Object element4="test4";
+//        SinglyLinkedList objList=new SinglyLinkedList();
+//        objList.appendNode(objList,element);
+//        objList.appendNode(objList,element2);
+//        objList.appendNode(objList,element3);
+//        objList.appendNode(objList,element4);
+//        //When
+//        Object actual=objList.getNode(objList,2);
+//        //Then
+//        Assert.assertEquals(actual,"test3");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void copyList() {
+        Object element="test";
+        Object element2="test2";
+        Object element3="test3";
+        Object element4="test4";
+        SinglyLinkedList objList=new SinglyLinkedList();
+        objList.appendNode(objList,element);
+        objList.appendNode(objList,element2);
+        objList.appendNode(objList,element3);
+        objList.appendNode(objList,element4);
+        SinglyLinkedList objDestinationList=new SinglyLinkedList();
+        //When
+        objList.copyList(objList);
+        String actual=objList.print(objDestinationList);
+        //Then
+        Assert.assertEquals("test test2 test3 test4 ",actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void sortList() {
+        Object element=1;
+        Object element2=6;
+        Object element3=2;
+        Object element4=5;
+        SinglyLinkedList objList=new SinglyLinkedList();
+        objList.appendNode(objList,element);
+        objList.appendNode(objList,element2);
+        objList.appendNode(objList,element3);
+        objList.appendNode(objList,element4);
+        //When
+        objList.sortList(objList);
+        String actual=objList.print(objList);
+        //Then
+        Assert.assertEquals("1 2 5 6 ",actual);
+    }
+
+    
 }
